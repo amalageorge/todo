6278c6a0ef93935607afaf0253428f226659d98d
Amala George <ammu@Amalas-MacBook-Pro.local>
2018-08-07 22:44:30 +0530

    modified

--- app.py
+++ app.py
@@ -10,5 +10,33 @@ def prompt_user_input():
 
 input = prompt_user_input()
 
+entries = []
+
+def add_entry():
+    print "Enter the item"
+    entry = raw_input()
+    entries.append(entry)
+
+count = len(entries)
+
+def delete_entry():
+    print "Enter the element to be deleted"
+    to_delete = raw_input()
+    entries.remove(to_delete)
+
 while input != '5':
+
+    if input == '1':
+        add_entry()
+        print (entries)
+
+    elif input == '3':
+        delete_entry()
+        print (entries)
+
+    elif input == '4':
+        print (entries)
+
     input = prompt_user_input()
+
+
